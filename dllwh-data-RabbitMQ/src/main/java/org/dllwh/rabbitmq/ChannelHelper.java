c3af4649bc2412e1eb2123a93de8c1fa65c14e76
独泪了无痕 <duleilewuhen@sina.com>
2019-07-23 01:35:48 +0800

    信道工具类

---
+++ dllwh-data-RabbitMQ/src/main/java/org/dllwh/rabbitmq/ChannelHelper.java
@@ -0,0 +1,40 @@
+package org.dllwh.rabbitmq;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 信道工具类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2019年7月23日 上午1:28:51
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+public class ChannelHelper {
+	/**
+	 * 
+	 * @方法描述: 绑定一个队列到一个匹配型交换器使用一个routingKey
+	 * @param queue
+	 * @param exchange
+	 * @param routingKey
+	 */
+//	public void addBinding(Queue queue, TopicExchange exchange, String routingKey) {
+//	}
+
+	/**
+	 * @方法描述: 绑定一个Exchange到一个匹配型Exchange 使用一个routingKey
+	 * @param exchange
+	 * @param topicExchange
+	 * @param routingKey
+	 */
+//	public void addBinding(Exchange exchange, TopicExchange topicExchange, String routingKey) {
+//	}
+
+	/**
+	 * @方法描述: 去掉一个binding
+	 * @param binding
+	 */
+//	public void removeBinding(BindOk binding) {
+//	}
+}
