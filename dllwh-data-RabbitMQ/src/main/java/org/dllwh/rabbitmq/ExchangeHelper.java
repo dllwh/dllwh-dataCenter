3e7024345f3768caad56867b459aab50974aa0a1
独泪了无痕 <duleilewuhen@sina.com>
2019-07-23 01:35:18 +0800

    交换机工具类

---
+++ dllwh-data-RabbitMQ/src/main/java/org/dllwh/rabbitmq/ExchangeHelper.java
@@ -0,0 +1,31 @@
+package org.dllwh.rabbitmq;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 交换机工具类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2019年7月23日 上午1:23:53
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+public final class ExchangeHelper {
+	/**
+	 * @方法描述: 创建Exchange
+	 * @param exchange
+	 */
+	public static void addExchange(String exchange) {
+
+	}
+
+	/**
+	 * @方法描述: 删除一个Exchange
+	 * @param exchangeName
+	 * @return
+	 */
+	public static boolean deleteExchange(String exchangeName) {
+		return false;
+	}
+}
