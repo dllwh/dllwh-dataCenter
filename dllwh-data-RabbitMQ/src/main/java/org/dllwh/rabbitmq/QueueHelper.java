c87c8316cc32c6cacb68da8596a12b15d420d20d
独泪了无痕 <duleilewuhen@sina.com>
2019-07-23 01:35:34 +0800

    队列工作类

---
+++ dllwh-data-RabbitMQ/src/main/java/org/dllwh/rabbitmq/QueueHelper.java
@@ -0,0 +1,43 @@
+package org.dllwh.rabbitmq;
+
+import com.rabbitmq.client.AMQP.Queue;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 队列工作类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2019年7月23日 上午1:25:27
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+public final class QueueHelper {
+	public static Queue addQueue() {
+		return null;
+	}
+
+	/**
+	 * 
+	 * @方法描述: 创建一个指定的Queue
+	 * @param queue
+	 * @return
+	 */
+	public static String addQueue(Queue queue) {
+		return null;
+	}
+
+	/**
+	 * @方法描述: 删除一个queue
+	 * @param queueName
+	 * @return
+	 */
+	public static boolean deleteQueue(String queueName) {
+		return false;
+	}
+
+	public void deleteQueue(String queueName, boolean unused, boolean empty) {
+
+	}
+}
