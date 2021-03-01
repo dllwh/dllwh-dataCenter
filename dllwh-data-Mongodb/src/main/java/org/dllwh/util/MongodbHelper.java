9a564fd8ccb1d3546c592efc09630d5d20cb7289
独泪了无痕 <duleilewuhen@sina.com>
2021-03-01 21:43:36 +0800

    构建项目

---
+++ dllwh-data-Mongodb/src/main/java/org/dllwh/util/MongodbHelper.java
@@ -0,0 +1,18 @@
+package org.dllwh.util;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: Mongodb帮助类
+ * 
+ * @author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @创建时间: 2021-03-01 21:39:17
+ * @版本: V 1.0.1
+ * @since: JDK 1.8
+ *
+ */
+public class MongodbHelper {
+
+}
