60c00ad31e78867b37f62371fe29637295a16f57
独泪了无痕 <duleilewuhen@sina.com>
2024-09-21 17:22:39 +0800

    创建MongoDB 项目模块

---
+++ dllwh-data-Mongodb/SpringBoot3-MongoDB/src/main/java/org/dllwh/mongodb/MongoApplication.java
@@ -0,0 +1,18 @@
+package org.dllwh.mongodb;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * <p>
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @ClassName MongoApplication
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @Author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @Date 2024-09-21 17:21
+ * @版本 V 1.0.1
+ * @see <a href="">TODO(连接内容简介)</a>
+ * @since JDK 1.8
+ */
+public class MongoApplication {
+
+}
