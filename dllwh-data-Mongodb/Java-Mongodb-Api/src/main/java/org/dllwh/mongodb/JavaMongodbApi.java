5cd6487806b20617c2a40afb5ba99fc9d9547aaa
独泪了无痕 <duleilewuhen@sina.com>
2024-09-21 16:06:05 +0800

    创建ElasticSearch、MongoDB 项目模块

---
+++ dllwh-data-Mongodb/Java-Mongodb-Api/src/main/java/org/dllwh/mongodb/JavaMongodbApi.java
@@ -0,0 +1,17 @@
+package org.dllwh.mongodb;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * <p>
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @ClassName JavaMongodbApi
+ * @Description Java 整合 mongodb 以及基本操作
+ * @Author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @Date 2024-09-21 16:02
+ * @版本 V 1.0.1
+ * @since JDK 1.8
+ */
+public class JavaMongodbApi {
+
+}
