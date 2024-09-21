5ac1e50ea27fd7fd588d879d037e77b464b08edd
独泪了无痕 <duleilewuhen@sina.com>
2024-09-21 17:30:07 +0800

    SpringBoot 整合 ElasticSearch

---
+++ dllwh-data-Elasticsearch/SpringBoot-ElasticSearch/src/main/java/org/dllwh/elastic/ElasticsearchApplication.java
@@ -0,0 +1,18 @@
+package org.dllwh.elastic;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * <p>
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @ClassName ElasticsearchApp
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @Author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @Date 2024-09-21 17:27
+ * @版本 V 1.0.1
+ * @see <a href="">TODO(连接内容简介)</a>
+ * @since JDK 1.8
+ */
+public class ElasticsearchApplication {
+
+}
